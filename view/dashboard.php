a799a40bdd5496cad3b65a3bc209b878fd95db05
Mundovirtual <38699384+Mundovirtual@users.noreply.github.com>
2018-09-02 20:48:57 -0500

    Diseño completado Dashboard

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,22 +1,18 @@
 <?php
   include_once("../modulos/enrutador.php");
   include_once("../modulos/controlador.php");
-
-
 ?>
  <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Bootstrap Material Admin by Bootstrapious.com</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="robots" content="all,follow">
+    <title>Admin</title>
+    
     <!-- Bootstrap CSS--> 
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.css">
     <!-- Fontastic Custom icon font-->
     <link rel="stylesheet" href="../css/fontastic.css">
     <!-- Google fonts - Poppins -->
@@ -29,12 +25,12 @@
     <link rel="shortcut icon" href="../img/favicon.apple-icon.png">
     
   </head>
+   
   <body>
     <div class="page">
       <!-- Main Navbar-->
       <header class="header">
-        <nav class="navbar">
-          
+        <nav class="navbar">          
           <div class="container-fluid">
             <div class="navbar-holder d-flex align-items-center justify-content-between">
               <!-- Navbar Header-->
@@ -46,8 +42,7 @@
                 <!-- Toggle Button--><a id="toggle-btn" href="#" class="menu-btn active"><span></span><span></span><span></span></a>
               </div>
               <!-- Navbar Menu -->
-              <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
-                                 
+              <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">            
                 <!-- Logout    -->
                 <li class="nav-item"><a href="login.html" class="nav-link logout"> <span class="d-none d-sm-inline">Logout</span><i class="fa fa-sign-out"></i></a></li>
               </ul>
@@ -55,6 +50,7 @@
           </div>
         </nav>
       </header>
+      
       <div class="page-content d-flex align-items-stretch"> 
         <!-- Side Navbar -->
         <nav class="side-navbar">
@@ -69,52 +65,42 @@
           <!-- Sidebar Navidation Menus-->
           <span class="heading">Main</span>
           <ul class="list-unstyled">
-                    <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
-                    <li><a href="tables.html"> <i class="icon-grid"></i>Tables </a></li>
-                    <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
-                    <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
-                    <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-interface-windows"></i>Registro </a>
-                      <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                        <li><a href="#">Hackaton</a></li>
-                        <li><a href="#">Vertical</a></li>
-                        <li><a href="#">Page</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="login.html"> <i class="icon-interface-windows"></i>Login page </a></li>
-           
+             
+            <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-interface-windows"></i>Registro </a>
+              <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+                <li><a href="#">Hackaton</a></li>
+                <li><a href="#">Vertical</a></li>
+                <li><a href="#">Page</a></li>
+              </ul>
+            </li>
+            <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
+            <li><a href="login.html"> <i class="icon-interface-windows"></i>Login page </a></li>
+          </ul>           
         </nav>
         <div class="content-inner">
-          <!-- Page Header-->
-          <header class="page-header">
-            <div class="container-fluid">
-              <h2 class="no-margin-bottom">Dashboard</h2>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12">
+                 <section class="dashboard-counts no-padding-bottom">
+                  <?php  
+                    $enrutador =new enrutador();                    
+                        
+                    if ($enrutador->validarGET($_GET['cargar'])) { 
+                          $enrutador->cargarVista($_GET['cargar']);
+                       } 
+                  ?>             
+                </section> 
+              </div> 
             </div>
-          </header>
-          <!-- Dashboard Counts Section-->
-          <section class="dashboard-counts no-padding-bottom">
-             
-               <?php  
-                  $enrutador =new enrutador();                    
-                      
-                  if ($enrutador->validarGET($_GET['cargar'])) { 
-                        $enrutador->cargarVista($_GET['cargar']);
-                     } 
-                ?> 
-           
-          </section>
-  
+          </div>
         </div>
       </div>
     </div>
     <!-- JavaScript files-->
     <script src="../js/jquery/jquery.min.js"></script> 
     <script src="../js/Bootstrap/bootstrap.min.js"></script> 
-  
-
-
-
- 
   </body>    
 
 </html> 
  
+
