eea47c21533a9604ada7affa3bb19e56af9df6a9
Mundovirtual <38699384+Mundovirtual@users.noreply.github.com>
2018-09-09 08:03:19 -0500

    RouterEquipoCorregido

--- modulos/enrutador.php
+++ modulos/enrutador.php
@@ -4,12 +4,10 @@
  
 			switch ($vista) {
 			    case "1": 
-			        include_once("../view/hackaton.php");
-			         
+			        include_once("../view/hackaton.php");      
 			        break;
 			    case "2": 
-			        include_once("../view/Vertical.php");
-			         
+			        include_once("../view/Vertical.php");			         
 			        break;
 			    case "3": 
 			        include_once("../view/confHackaton.php");			         
@@ -17,13 +15,15 @@
 		        case "4": 
 			        include_once("../view/Rubricas.php");			         
 		        break;
-		         case "5": 
+		        case "5": 
 			        include_once("../view/jueces.php");			         
+		        break;
+		        case "6": 
+			        include_once("../view/equipos.php");			         
 		        break;
 			    case "error":
 			    	include_once("../view/".$vista.".php");
 			    break;	
-
 			    default:	
 					include_once("../view/error.php");
 			}
