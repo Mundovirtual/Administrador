e4754d2ecf1fb25158dfd388a7db530b65c284c2
Mundovirtual <38699384+Mundovirtual@users.noreply.github.com>
2018-09-16 00:07:43 -0500

    BotonesValidados

--- view/panelControl.php
+++ view/panelControl.php
@@ -10,7 +10,7 @@
 		  <div class="card-header bg-success" align="center">Primera Fase</div>
 		  <div class="card-body"  align="center">
 		     
-		     <button id="faseUno" type="button" class="btn btn-primary" >Empezar</button>
+		     <button id="faseUno" type="button" class="btn btn-primary" ></button>
 
 		  </div>
 		</div>
@@ -22,7 +22,7 @@
 		  <div class="card-header bg-info" align="center">Segunda Fase</div>
 		  <div class="card-body"  align="center">
 		   
-		   <button id="faseDos" type="button" class="btn btn-success">Empezar</button>
+		   <button id="faseDos" type="button" class="btn btn-success"></button>
 
 		  </div>
 		</div>
@@ -34,7 +34,7 @@
 		<div  class="card text-white bg-dark mb-3" style="max-width: 18rem;">
 		  <div class="card-header bg-primary" align="center">Tercera Fase</div>
 		  <div class="card-body" align="center">
-		      <button id="faseTres" type="button" class="btn  btn-danger">Empezar</button>	   
+		      <button id="faseTres" type="button" class="btn  btn-danger"></button>	   
 			</div>
 		</div>
 		 
@@ -43,77 +43,80 @@
 </div>
 
  <script type="text/javascript">
+ 	
+	var botonUno="Finalizado";
+	var botonDos="Finalizado";
+	var botonTres="Finalizado";
 
- 	var EstadoBotfaseUno ="Empezar";
- 	var EstadoBotfaseUno ="Finalizar";
- 	var EstadoBotfaseUno ="Finalizado";
 
- 	alert(EstadoBotfaseUno);
- 	alert(EstadoBotfaseUno=="Empezar");
+	$('#faseUno').attr('disabled', true);
+	$('#faseDos').attr('disabled', true);		
+	$('#faseTres').attr('disabled', true);
+ 	
+	$('#faseUno').text(botonUno);
+	$('#faseDos').text(botonDos);
+	$('#faseTres').text(botonTres);
 
  	
- 	if (EstadoBotfaseUno=="Empezar") {
-    	$('#faseUno').text("Finalizar");
-    	//Boton fase dos
-    	$('#faseDos').attr('disabled', false);
-    	//Boton fase tres
-    	$('#faseTres').attr('disabled', false);
+	if ($("#faseUno").text()=="Empezar") {
+		$('#faseUno').attr('disabled', false); 
+		
+		$("#faseUno").click(function(){ 
+	    		$('#faseUno').text("Finalizar");
+	    		//Update base de datos tabla: InfConfiguracion-> Status::: Estado Finalizar
+	     });
 
- 	}else if(EstadoBotfaseUno="Finalizar"){
+	} else if($("#faseUno").text()=="Finalizar") {
+		$('#faseUno').attr('disabled', false); 
+		
+		$("#faseUno").click(function(){ 
+	    		$('#faseUno').text("Finalizado");
+	    		//Update base de datos tabla: InfConfiguracion-> Status::: Estado Finalizado
+	     });
+	}else if ($("#faseUno").text()=="Finalizado") {
+		$('#faseDos').attr('disabled', true);  
+	}
 
- 	}else{
+	//Fase dos
+	if ($("#faseDos").text()=="Empezar") {
+		$('#faseDos').attr('disabled', false); 
+		
+		$("#faseDos").click(function(){ 
+	    		$('#faseDos').text("Finalizar");
+	    		//Update base de datos tabla: InfConfiguracion-> Status::: Estado Finalizar
+	     });
 
- 	}
+	} else if($("#faseDos").text()=="Finalizar") {
+		$('#faseDos').attr('disabled', false); 
+		
+		$("#faseDos").click(function(){ 
+	    		$('#faseDos').text("Finalizado");
+	    		//Update base de datos tabla: InfConfiguracion-> Status::: Estado Finalizado
+	     });
+	}else if ( $("#faseDos").text()=="Finalizado") {
+		$('#faseTres').attr('disabled', false);
+	}
 
+	//Fase Tres
+	if ($("#faseTres").text()=="Empezar") {
+		$('#faseTres').attr('disabled', false); 
+		
+		$("#faseTres").click(function(){ 
+	    		$('#faseTres').text("Finalizar");
+	    		//Update base de datos tabla: InfConfiguracion-> Status::: Estado Finalizar
+	     });
 
-
-
-
-
-
-
-
-
-
- 	/*Botones*/
- 	/*
-
- 	$("#faseUno").click(function(){
-    	$('#faseUno').attr('disabled', true);
-    	$('#faseUno').text("Finalizado");
-    	fu=1;
-
-    	//Boton fase dos
-    	$('#faseDos').attr('disabled', false);
-    	//Boton fase tres
-    	$('#faseTres').attr('disabled', true);
-
-    });
-
-    $("#faseDos").click(function(){
-    	$('#faseDos').attr('disabled', true);
-    	$('#faseDos').text("Finalizado");
-    	fd=1;
-
-    	//Boton fase dos
-    	$('#faseDos').attr('disabled', true);
-    	//Boton fase tres
-    	$('#faseTres').attr('disabled', false);
-
-    });
-
-    $("#faseTres").click(function(){
-    	$('#faseTres').attr('disabled', true);
-    	$('#faseTres').text("Finalizado");
-    	ft=1;
-
-    	//Boton fase dos
-    	$('#faseUno').attr('disabled', true);
-    	//Boton fase tres
-    	$('#faseDos').attr('disabled', true);
-    });
+	} else if($("#faseTres").text()=="Finalizar") {
+		$('#faseTres').attr('disabled', false); 
+		
+		$("#faseTres").click(function(){ 
+	    		$('#faseTres').text("Finalizado");
+	    		//Update base de datos tabla: InfConfiguracion-> Status::: Estado Finalizado
+	     });
+	}else if ( $("#faseTres").text()=="Finalizado") {
+		$('#faseTres').attr('disabled', true); 
+	}
  
- 	
 
  	 
 	
